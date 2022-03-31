e321c5587774bf65e90cf3accdfa7bad6ba24f03
Guillermo Conde Flores <condeguillermo8@gmail.com>
2022-03-31 15:44:06 -0600

    Agregando toda la funcionalidad de la calculadora

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import styled from 'styled-components'
+import { useState } from 'react'
+import {Formik, Form} from 'formik'
+import * as Yup from 'yup'
+import Input from './components/Input'
+import Button from './components/Button'
+import Container from './components/Container'
+import Section from './components/Section'
+import Balance from './components/Balance'
 
-const Container = styled.div`
-display: flex;
-justify-content: center;
-height: 100%;
-align-items: center;
-`
-const Section = styled.section`
-background: #eee;
-border-top: solid 2px palevioletred;
-padding: 20px 25px;
-width: 500px;
-box-shadow 0px 2px 3px rgb(0,0,0,0.3);`
+const compundInterest = (deposit, contribution, years, rate) => {
+  let total = deposit
+  for (let i = 0; i < years; i++) {
+        total = (total + contribution) * (rate + 1)
+  }
+  return Math.round(total);
+}
+
+const formatter = new Intl.NumberFormat('en-US',{
+  style: 'currency',
+  currency: 'USD',
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2
+})
 
 function App() {
+  const [balance, setBalance] = useState('')
+  const handleSubmit = ({deposit, contribution, years, rate}) => {
+    const val = compundInterest(Number(deposit), Number(contribution), Number(years), Number(rate))
+    setBalance(formatter.format(val));
+  }
+
   return (
     <Container>
       <Section>
-        lala
+        <Formik
+          initialValues={{
+            deposit: '',
+            contribution: '',
+            years: '',
+            rate: '',
+          }}
+          onSubmit={handleSubmit}
+          validationSchema={Yup.object({
+            deposit: Yup.number().required('Este dato es requerido').typeError('Debe ingresar un número'),
+            contribution: Yup.number().required('Este dato es requerido').typeError('Debe ingresar un número'),
+            years: Yup.number().required('Este dato es requerido').typeError('Debe ingresar un número'),
+            rate: Yup.number().required('Este dato es requerido').typeError('Debe ingresar un número').min(0, 'El valor debe ser mayor o igual a 0').max(1, 'El valor debe ser menor o igual a 1'),
+          })}
+          >
+            <Form>
+              <Input name="deposit" label="Depósito inicial"></Input>
+              <Input name="contribution" label="Contribución"></Input>
+              <Input name="years" label="Años"></Input>
+              <Input name="rate" label="Interés estimado"></Input>
+              <Button type='submit'>Calcular</Button>
+            </Form>
+        </Formik>
+        {balance !== '' ? <Balance>El balance final es:  {balance}</Balance>:null}
       </Section>
     </Container>
   );
